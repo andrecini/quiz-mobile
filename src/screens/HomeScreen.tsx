f80b3fdcdf839410007b217760d66f7a5c19b2fd
André Cini <andrevpcini@gmail.com>
2024-09-22 12:54:41 -0300

    [STYLE] - Ajuste na tela inicial

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { View, Button, Text } from 'react-native';
-import { styles } from '../styles/style';
-import { NavigationProp } from '@react-navigation/native';
-import { Theme } from '../styles/Theme';
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons"; // Usando Ionicons para os ícones
+import { NavigationProp } from "@react-navigation/native";
+import { Theme } from "../styles/Theme";
+import { styles } from "../styles/style"; // Garanta que o arquivo styles tenha os ajustes
+import { VStack } from "native-base";
 
 interface HomeScreenProps {
   navigation: NavigationProp<any>;
@@ -10,34 +12,136 @@ interface HomeScreenProps {
 
 export const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
   return (
-    <View style={[styles.container, { backgroundColor: Theme.colors.background }]}>
-      <Text style={[styles.title, { color: Theme.colors.textPrimary }]}>
-        Bem vindo ao Quiz!
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: Theme.colors.background, justifyContent: "flex-start" },
+      ]}
+    >
+      <Text
+        style={[
+          styles.title,
+          {
+            color: Theme.colors.textPrimary,
+            fontSize: 34,
+            fontWeight: "bold",
+            marginBottom: 40,
+            justifyContent: "center",
+          },
+        ]}
+      >
+        Bem vindo ao Quiz App
       </Text>
 
-      <View style={styles.buttonContainer}>
-        <Button
-          title="Temas"
-          onPress={() => navigation.navigate('AddThemeScreen')}
-          color={Theme.colors.primary}
-        />
-      </View>
+      <Text
+        style={[
+          styles.title,
+          {
+            color: Theme.colors.textPrimary,
+            fontSize: 24,
+            fontWeight: "bold",
+            marginBottom: 10,
+            justifyContent: "center",
+          },
+        ]}
+      >
+        Gerenciamento
+      </Text>
 
-      <View style={styles.buttonContainer}>
-        <Button
-          title="Perguntas"
-          onPress={() => navigation.navigate('AddQuestionScreen')}
-          color={Theme.colors.primary}
-        />
-      </View>
+      <VStack
+        space={4}
+        style={{ alignItems: "center", justifyContent: "center" }}
+      >
+        <TouchableOpacity
+          style={[
+            {
+              backgroundColor: Theme.colors.primary,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: 15,
+              borderRadius: 10,
+              width: '100%'
+            },
+          ]}
+          onPress={() => navigation.navigate("AddThemeScreen")}
+        >
+          <Ionicons
+            name="albums"
+            size={24}
+            color={Theme.colors.textPrimary}
+            style={{ marginRight: 10 }}
+          />
+          <Text style={{ color: Theme.colors.textPrimary }}>Temas</Text>
+        </TouchableOpacity>
 
-      <View style={styles.buttonContainer}>
-        <Button
-          title="Jogar"
-          onPress={() => navigation.navigate('PlayQuizScreen')}
-          color={Theme.colors.primary}
-        />
-      </View>
+        <TouchableOpacity
+          style={[
+            {
+              backgroundColor: Theme.colors.primary,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: 15,
+              borderRadius: 10,
+              width: '100%'
+            },
+          ]}
+          onPress={() => navigation.navigate("AddThemeScreen")}
+        >
+          <Ionicons
+            name="help-circle"
+            size={24}
+            color={Theme.colors.textPrimary}
+            style={{ marginRight: 10 }}
+          />
+          <Text style={{ color: Theme.colors.textPrimary }}>Perguntas</Text>
+        </TouchableOpacity>
+      </VStack>
+      
+      <Text
+        style={[
+          styles.title,
+          {
+            color: Theme.colors.textPrimary,
+            fontSize: 24,
+            fontWeight: "bold",
+            marginBottom: 10,
+            marginTop: 10,
+            justifyContent: "center",
+          },
+        ]}
+      >
+        Game
+      </Text>
+      
+      <VStack
+        space={4}
+        style={{ alignItems: "center", justifyContent: "center" }}
+      >
+        <TouchableOpacity
+          style={[
+            {
+              backgroundColor: Theme.colors.primary,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: 15,
+              borderRadius: 10,
+              width: '100%'
+            },
+          ]}
+          onPress={() => navigation.navigate("AddThemeScreen")}
+        >
+          <Ionicons
+            name="play"
+            size={24}
+            color={Theme.colors.textPrimary}
+            style={{ marginRight: 10 }}
+          />
+          <Text style={{ color: Theme.colors.textPrimary }}>Jogar</Text>
+        </TouchableOpacity>
+      </VStack>
     </View>
   );
 };
